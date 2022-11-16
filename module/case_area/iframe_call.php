0f2824194ff28b0752e39e0070e7c6fb09cb034c
d974252037 <a45126812000@gmail.com>
2022-11-16 09:27:48 +0800

    更新聯絡我們，增加自訂欄位方法

--- module/case_area/iframe_call.php
+++ module/case_area/iframe_call.php
@@ -1,4 +1,7 @@
 <?php include("../../core/page/header01.php");//載入頁面heaer01 ?>
+<link rel="stylesheet" type="text/css" href="../../css/codemirror.css">
+<link rel="stylesheet" type="text/css" href="../../js/plugins/codemirror/theme/monokai.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/monokai.min.css">
 <style type="text/css">
 	.md-skin .navbar-static-side, .border-bottom, body.fixed-sidebar .navbar-static-side, body.canvas-menu .navbar-static-side{display: none;}
 	#page-wrapper{ margin:0px;  }
@@ -40,6 +43,7 @@ if($_POST){
     $Tb_index='call'.date('YmdHis').rand(0,99);
 
     $OnLineOrNot=empty($_POST['OnLineOrNot'])? 0:1;
+    $is_custom=empty($_POST['is_custom'])? 0:1;
 
     //---- 更新關聯資料表 -----
     pdo_update('Related_tb', ['fun_id'=>$Tb_index, 'OnLineOrNot'=>$OnLineOrNot], ['Tb_index'=>$_GET['rel_id']]);
@@ -52,6 +56,9 @@ if($_POST){
        're_mail'=>implode(',', $_POST['re_mail']),
        'send_type'=>$_POST['send_type'],
        'line_Client_ID'=>$_POST['line_Client_ID'],
+       'is_custom'=>$is_custom,
+       'cus_html'=>$_POST['cus_html'],
+       'thanks_url'=>$_POST['thanks_url'],
        'OnLineOrNot'=>$OnLineOrNot
     ];
 
@@ -71,12 +78,16 @@ if($_POST){
     
 
       $OnLineOrNot=empty($_POST['OnLineOrNot'])? 0:1;
+      $is_custom=empty($_POST['is_custom'])? 0:1;
       $param=[
        'btn_name'=>$_POST['btn_name'],
        're_name'=>implode(',', $_POST['re_name']),
        're_mail'=>implode(',', $_POST['re_mail']),
        'send_type'=>$_POST['send_type'],
        'line_Client_ID'=>$_POST['line_Client_ID'],
+       'is_custom'=>$is_custom,
+       'cus_html'=>$_POST['cus_html'],
+       'thanks_url'=>$_POST['thanks_url'],
        'OnLineOrNot'=>$OnLineOrNot
     ];
 
@@ -119,116 +130,167 @@ if($_POST){
       
 	</div>
 	<div class="row" >
-		<div class="col-lg-12">
-			<div class="ibox float-e-margins">
-			 <div class="ibox-title">
-			 	
-			 	<div class="ibox-tools" >
-			 		<button id="save_btn" type="button" class="btn btn-primary">儲存</button>      
-			 	</div>
-			 </div>
-			<div class="ibox-content">
-				<form id="fun_form" action="#" method="POST" class="form-horizontal" enctype='multipart/form-data'>
-
-          <div class="form-group">
-              <label class="col-sm-2 control-label">按鈕名稱</label>
-              <div class="col-sm-10">
-                <input type="text" class="form-control" name="btn_name" value="<?php echo $row['btn_name'];?>">
-              </div>
-              
-            </div>
-				   
-          <?php 
-           for ($i=0; $i <8 ; $i++) { 
-          ?>
-
-            <div class="form-group">
-              <label class="col-sm-2 control-label">收件人</label>
-              <div class="col-sm-4">
-                <input type="text" class="form-control" name="re_name[]" value="<?php echo $re_name[$i];?>">
-              </div>
-
-              <label class="col-sm-2 control-label">收件人E-mail</label>
-              <div class="col-sm-4">
-                <input type="text" class="form-control" name="re_mail[]" value="<?php echo $re_mail[$i];?>">
-              </div>
-            </div>
-
-          <?php
-           }
-          ?>
-           
-            <div class="form-group">
-              <label class="col-sm-2 control-label" for="send_type">發信類型</label>
-              <div class="col-sm-10">
-                <label> <input type="radio" name="send_type" value="0" checked> 系統查看 </label>｜
-                <label> <input type="radio" name="send_type" value="1"> 直接顯示資料 </label>
-                <input type="hidden" name="ch_send_type" value="<?php echo $row['send_type'];?>">
-              </div>
-            </div>
-
-            <div class="form-group" <?php echo $btn_display;?>>
-                <label class="col-sm-2 control-label" ></label>
-                <div class="col-sm-10">
-                  <a id="update_notify_btn" class="btn btn-success" href="javascript:;">更新LINE notify</a>
-                </div>
-            </div>
-            <div class="notify_box" <?php echo $box_display;?>>
-              <div class="form-group">
-                <label class="col-sm-2 control-label" for="line_Client_ID">LINE notify Client ID</label>
-                <div class="col-sm-4">
-                  <input type="text" class="form-control" name="line_Client_ID" value="<?php echo $row['line_Client_ID'];?>">
-                </div>
-                <label class="col-sm-2 control-label" for="line_Client_Secret">LINE notify Client Secret</label>
-                <div class="col-sm-4">
-                  <input type="text" class="form-control" name="line_Client_Secret" value="">
+    <form id="fun_form" action="#" method="POST"  enctype='multipart/form-data'>
+            <div class="col-lg-12">
+              <div class="ibox float-e-margins">
+              <div class="ibox-title">
+                
+                <div class="ibox-tools" >
+                  <button id="save_btn" type="button" class="btn btn-primary">儲存</button>      
                 </div>
               </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label" for="send_type">連接LINE notify</label>
-                <div class="col-sm-10">
+              <div class="ibox-content">
+                <div class="form-horizontal" >
+
+                  <div class="form-group">
+                      <label class="col-sm-2 control-label">按鈕名稱</label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" name="btn_name" value="<?php echo $row['btn_name'];?>">
+                      </div>
+                      
+                    </div>
+                  
+                  <?php 
+                  for ($i=0; $i <8 ; $i++) { 
+                  ?>
+
+                    <div class="form-group">
+                      <label class="col-sm-2 control-label">收件人</label>
+                      <div class="col-sm-4">
+                        <input type="text" class="form-control" name="re_name[]" value="<?php echo $re_name[$i];?>">
+                      </div>
+
+                      <label class="col-sm-2 control-label">收件人E-mail</label>
+                      <div class="col-sm-4">
+                        <input type="text" class="form-control" name="re_mail[]" value="<?php echo $re_mail[$i];?>">
+                      </div>
+                    </div>
+
                   <?php
-                   if(empty($row['line_notify_token']) && !empty($row['line_Client_ID'])){
-                     echo '<a target="_blank" class="btn btn-success" href="https://notify-bot.line.me/oauth/authorize?response_type=code&client_id='.$row['line_Client_ID'].'&redirect_uri=https://ws.srl.tw/line_notify/notify.php&scope=notify&state='.$_GET['Tb_index'].'">連結</a>';
-                   }
-                   elseif(!empty($row['line_notify_token']) && !empty($row['line_Client_ID'])){
-                     echo '<a class="btn btn-danger del_line_btn" href="javascript:;">取消連結</a>';
-                   }
+                  }
                   ?>
                   
+                    <div class="form-group">
+                      <label class="col-sm-2 control-label" for="send_type">發信類型</label>
+                      <div class="col-sm-10">
+                        <label> <input type="radio" name="send_type" value="0" checked> 系統查看 </label>｜
+                        <label> <input type="radio" name="send_type" value="1"> 直接顯示資料 </label>
+                        <input type="hidden" name="ch_send_type" value="<?php echo $row['send_type'];?>">
+                      </div>
+                    </div>
+
+                    <div class="form-group" <?php echo $btn_display;?>>
+                        <label class="col-sm-2 control-label" ></label>
+                        <div class="col-sm-10">
+                          <a id="update_notify_btn" class="btn btn-success" href="javascript:;">更新LINE notify</a>
+                        </div>
+                    </div>
+                    <div class="notify_box" <?php echo $box_display;?>>
+                      <div class="form-group">
+                        <label class="col-sm-2 control-label" for="line_Client_ID">LINE notify Client ID</label>
+                        <div class="col-sm-4">
+                          <input type="text" class="form-control" name="line_Client_ID" value="<?php echo $row['line_Client_ID'];?>">
+                        </div>
+                        <label class="col-sm-2 control-label" for="line_Client_Secret">LINE notify Client Secret</label>
+                        <div class="col-sm-4">
+                          <input type="text" class="form-control" name="line_Client_Secret" value="">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="col-sm-2 control-label" for="send_type">連接LINE notify</label>
+                        <div class="col-sm-10">
+                          <?php
+                          if(empty($row['line_notify_token']) && !empty($row['line_Client_ID'])){
+                            echo '<a target="_blank" class="btn btn-success" href="https://notify-bot.line.me/oauth/authorize?response_type=code&client_id='.$row['line_Client_ID'].'&redirect_uri=https://ws.srl.tw/line_notify/notify.php&scope=notify&state='.$_GET['Tb_index'].'">連結</a>';
+                          }
+                          elseif(!empty($row['line_notify_token']) && !empty($row['line_Client_ID'])){
+                            echo '<a class="btn btn-danger del_line_btn" href="javascript:;">取消連結</a>';
+                          }
+                          ?>
+                          
+                        </div>
+                      </div>
+                    </div>
+
+                    
+                    <div class="form-group" <?php echo $btn_display;?>>
+                      <label class="col-sm-2 control-label" for="send_type">LINE notify 發送測試</label>
+                      <div class="col-sm-4">
+                        <textarea name="send_test" id="send_test" class="form-control" rows="5"></textarea>
+                      </div>
+                      <div class="col-sm-2"><a class="btn btn-success send_test_btn" href="javascript:;">發送測試</a></div>
+                    </div>
+
+                    <div class="form-group">
+                      <label class="col-sm-2 control-label" for="OnLineOrNot">是否上線</label>
+                      <div class="col-sm-10">
+                        <input style="width: 20px; height: 20px;" id="OnLineOrNot" name="OnLineOrNot" type="checkbox" value="1" <?php echo $check=!isset($row['OnLineOrNot']) || $row['OnLineOrNot']==1 ? 'checked' : ''; ?>  />
+                      </div>
+                    </div>
                 </div>
+
+                <input type="hidden" id="Tb_index" value="<?php echo $_GET['Tb_index'];?>">
+                <input type="hidden" id="fun_id" value="<?php echo $_GET['fun_id'];?>">
               </div>
             </div>
-
-            
-            <div class="form-group" <?php echo $btn_display;?>>
-              <label class="col-sm-2 control-label" for="send_type">LINE notify 發送測試</label>
-              <div class="col-sm-4">
-                <textarea name="send_test" id="send_test" class="form-control" rows="5"></textarea>
-              </div>
-              <div class="col-sm-2"><a class="btn btn-success send_test_btn" href="javascript:;">發送測試</a></div>
-            </div>
-
-            <div class="form-group">
-              <label class="col-sm-2 control-label" for="OnLineOrNot">是否上線</label>
-              <div class="col-sm-10">
-                <input style="width: 20px; height: 20px;" id="OnLineOrNot" name="OnLineOrNot" type="checkbox" value="1" <?php echo $check=!isset($row['OnLineOrNot']) || $row['OnLineOrNot']==1 ? 'checked' : ''; ?>  />
-              </div>
-            </div>
-				</form>
-
-        <input type="hidden" id="Tb_index" value="<?php echo $_GET['Tb_index'];?>">
-        <input type="hidden" id="fun_id" value="<?php echo $_GET['fun_id'];?>">
-			</div>
-		</div>
-	</div>
+          </div>
+          <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>自定義表單</h5>
+                            <div class="ibox-tools">
+                               <label ><input type="checkbox" name="is_custom" id="is_custom" value="1" <?php echo $check=!isset($row['is_custom']) || $row['is_custom']==1 ? 'checked' : ''; ?>> 是否自定義表單</label>
+                            </div>
+                        </div>
+                        <div class="ibox-content">
+                          <div class="form-horizontal" >
+                              <p>
+                                HTML規則：<br>
+                                必填項目input加上"required"的class，屬性加上err_name="欄位名稱"如：
+                                <pre><code class="required_code language-html hljs"></code></pre><br>
+                                <span class="text-danger">(必要項目)：</span><br>
+                                個資告知事項聲明核選input 如： <pre><code class="user_code language-html hljs"></code></pre>
+                                送出表單按鈕 如：<pre><code class="submit_code language-html hljs"></code></pre>
+                                姓名欄位input加上id="ca_name"<br>
+                                電話欄位input加上id="ca_phone"<br>
+                                信箱欄位input加上id="ca_email"<br>
+                                其他欄位input加上id="可自訂"，屬性加上input_name="欄位名稱"如：<br>
+                                <pre><code class="input_code language-html hljs"></code></pre><br>
+                              </p>
+                              <div class="form-group">
+                                <textarea id="cus_html" name="cus_html" class="form-control"  rows="30"><?php echo $row['cus_html'];?></textarea>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-2 control-label">感謝頁連結</label>
+                                  <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="thanks_url" name="thanks_url" value="<?php echo $row['thanks_url'];?>">
+                                  </div>
+                                </div>
+                          </div>
+                        </div>
+                    </div>
+          </div>
+    </form>
+		
 </div>
 
 
 </div><!-- /#page-content -->
 <?php  include("../../core/page/footer01.php");//載入頁面footer01.php?>
+<script type="text/javascript" src="../../js/plugins/codemirror/codemirror.js"></script>
+<script type="text/javascript" src="../../js/plugins/codemirror/mode/xml/xml.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
 <script type="text/javascript">
 	$(document).ready(function() {
+
+      let html = hljs.highlight('<input type="text" class="form-control required" err_name="姓名" id="user_name" name="user_name">', {language: 'html'}).value;
+      $('.required_code').html(html);
+          html = hljs.highlight('<input type="checkbox" id="ca_privacy"> 本人知悉並同意『<a target="_blank" href="privacy.html">個資告知事項聲明</a>』內容', {language: 'html'}).value;
+      $('.user_code').html(html);
+          html = hljs.highlight('<button id="sub_btn" type="button" >發送訊息</button>', {language: 'html'}).value;
+      $('.submit_code').html(html);
+          html = hljs.highlight('<input type="text" id="ca_test" input_name="測試欄位名稱">', {language: 'html'}).value;
+      $('.input_code').html(html);
         
       $(`[name="send_type"][value="${$('[name="ch_send_type"]').val()}"]`).prop('checked',true);
 
@@ -244,9 +306,27 @@ if($_POST){
           else{
              $('#fun_form').submit();
           }
-
-
       });
+
+      // var myModeSpec = {
+      //   name: "xml",
+      //   tags: {
+      //     style: [["type", /^text\/(x-)?scss$/, "text/x-scss"],
+      //             [null, null, "css"]],
+      //     custom: [[null, null, "customMode"]]
+      //   }
+      // };
+      var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('cus_html'), {
+          mode: 'text/html',
+          lineNumbers: true,
+              matchBrackets: true,
+              styleActiveLine: true,
+              theme: 'monokai'
+      });
+
+      myCodeMirror.setSize(null, 600);
+
+
 
       //------------------------------ 刪圖檔 ---------------------------------
       $(".one_del_file").click(function(event) { 
